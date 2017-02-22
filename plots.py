df9f22726b2094d7b28e1b5c816f9f3ad496cd7e
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-02-22 23:09:06 +0500

    Add colormap mesh plot of D(k_perp, omega)

--- plots.py
+++ plots.py
@@ -8,6 +8,7 @@ Source: https://pgi-jcns.fz-juelich.de/portal/pages/using-c-from-python.html
 
 import ctypes
 import matplotlib.pyplot as plt
+import numpy
 import plac
 
 c_functions = ctypes.CDLL('./libfunctions.so')          # Load the library
@@ -15,6 +16,7 @@ c_functions = ctypes.CDLL('./libfunctions.so')          # Load the library
 c_gamma_n_array = c_functions.Gamma_n_array
 c_I_n_array = c_functions.I_n_array
 c_D_array = c_functions.D_array
+c_D = c_functions.D
 
 c_gamma_n_array.argtypes = (ctypes.c_int, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.c_int)
 c_gamma_n_array.restype = None
@@ -24,6 +26,9 @@ c_I_n_array.restype = None
 
 c_D_array.argtypes = (ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.c_int)
 c_D_array.restype = None
+
+c_D.argtypes = (ctypes.c_double, ctypes.c_double)
+c_D.restype = ctypes.c_double
 
 
 def function_n_array(fn, n, xs):
@@ -69,6 +74,24 @@ def D_array(xs):
     return c_ys            # Python seems capable of handling the c-type array without converting to a standard Python list
 
 
+def D_mesh(k_perp, omega):
+    """
+    The function takes two arrays of values for k_perp and omega that define the
+    coordinates in the mesh.
+
+    It returns a 2D numpy array with the function c_D applied to these coordinates on the mesh.
+    """
+
+    D = numpy.zeros(shape=(len(omega), len(k_perp)))        # Create empty 2D numpy array
+
+    for i in range(len(omega)):
+        for j in range(len(k_perp)):
+
+            D[i][j] = c_D(k_perp[j], omega[i])      # Use c_D to populate the array
+
+    return D
+
+
 def plot_Gamma_n():
     """
     Plot graphs of Gamma_1, Gamma_2, and Gamma_3 from 0 to 4 on the same plot.
@@ -111,7 +134,7 @@ def plot_I_n():
     axes.set_ylim([-0.25, 3.25])
 
 
-def plot_D():
+def plot_D_omega():
     """
     Plot a graph of D(omega) (the dispersion relation).
     """
@@ -129,8 +152,31 @@ def plot_D():
     axes.set_ylim([-10, 10])
 
 
+def plot_D():
+    """
+    Plot a 2D graph of D(k_perp, omega) (the dispersion relation)
+    """
+
+    # Use numpy to create a 1D array of 200 values between 0 and 5
+    k_perp = numpy.linspace(0, 5, 200)
+    omega = numpy.linspace(0, 5, 200)
+
+    # Use meshgrid to create 2D arrays by repeating the vectors (1D arrays)
+    # k_perp and omega over and over again
+    # Every element of the 2D K now contains the corresponding value of k_perp
+    K, O = numpy.meshgrid(k_perp, omega)      # Create a mesh by performing the outer-product of the two arrays to get a 2D set of tuples
+    D = D_mesh(k_perp, omega)      # Apply c_D to the mesh points
+
+    fig, ax = plt.subplots()
+    p = ax.pcolor(K, O, D, cmap=plt.cm.RdBu, vmin=abs(D).min(), vmax=abs(D).max())
+
+    cb = fig.colorbar(p)
+
+
+
 def main(gamma_n: ("Plot Gamma_n for n = 1,2,3", "flag", "g"),
-         I_n: ("Plot I_n for n = 0,1,2", "flag", "i")):
+         I_n: ("Plot I_n for n = 0,1,2", "flag", "i"),
+         D_omega: ("Plot D(omega) with k_perp = 1 fixed", "flag", "o")):
 
     if gamma_n:
         plot_Gamma_n()
@@ -138,10 +184,14 @@ def main(gamma_n: ("Plot Gamma_n for n = 1,2,3", "flag", "g"),
     elif I_n:
         plot_I_n()
 
+    elif D_omega:
+        plot_D_omega()
+
     # Default option
     else:
         # plot_Gamma_n()
         # plot_I_n()
+        # plot_D_omega()
         plot_D()
 
     plt.legend()
