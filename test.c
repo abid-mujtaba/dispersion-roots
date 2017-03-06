68ef070eff2516dc67113e9accb5c84b5a3adde9
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-03-06 11:50:49 +0500

    Remove func.c and func.h

--- test.c
+++ test.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
-#include "func.h"
 
 
 int main(void)
 {
-        double x1 = 1.25;
-        double x2 = 1.5;
-        double x3 = 2.25;
-
-        printf("\nln(%.2f) = %.9f", x1, ln(x1));
-        printf("\nln(%.2f) = %.9f", x2, ln(x2));
-        printf("\nln(%.2f) = %.9f", x3, ln(x3));
         printf("\n\n");
 
         return 0;
