caee9e788694da24fac45a19e91f731c532513af
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-03-08 14:34:24 +0500

    Remove ifndef since it is not required

--- roots.h
+++ roots.h
@@ -2,9 +2,6 @@
  * Define root finding function prototypes and related constants.
  */
 
-#ifndef ROOTS_H
-#define ROOTS_H
-
 // Define the bracket limits for root finding
 #define ROOT_LO 0.0
 #define ROOT_HI 5.5
@@ -17,5 +14,3 @@
 
 
 int find_k_perp_roots_array(double slices[], double omega[], double roots[], int size);
-
-#endif
