b69388e749dc958ed3d67d3ac2137d2e2866ca75
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-03-08 11:51:26 +0500

    Modify hypergeom functions to consolidate iteration calculations

--- hypergeom.c
+++ hypergeom.c
@@ -2,7 +2,6 @@
  * Define the 1F2 and 2F3 generalized hypergeometric functions required by the dispersion relation.
  */
 
-#include <stdio.h>
 #include <math.h>
 #include "hypergeom.h"
 
@@ -11,31 +10,17 @@ double hyp1F2(const double a1, const double b1, const double b2, const double x)
 {
         int k;
 
-        // Define variables that will contain the running value of the Pochhammer symbols corresponding to the a_i and b_i
-        double pa1 = 1;         // (a1)_0 = 1 by the definition of the Pochhammer symbol
-        double pb1 = 1;
-        double pb2 = 1;
-
-        // Define running variables for power of x and factorial
-        double fact = 1;
-        double pow_x = 1;
 
         double result = 0;
-        double prev = -1;
+        double term = 1;                // Stores the running value of each term in the summation. From the definition of the Pochhammer symbols the value of the k = 0 terms is ONE
 
-        for (k = 0; (k < MAX_TERMS) & (fabs(result - prev) > TOLERANCE); ++k)
+        // When (the absolute value of) 'term' becomes less than TOLERANCE the sum stops changing rapidly and we truncate it
+        for (k = 0; (k < MAX_TERMS) & (fabs(term) > TOLERANCE); ++k)
         {
-                prev = result;                  // Store previous value of result so the difference can be calculated and compared to the TOLERANCE on the next ieration
+                result += term;
 
-                result += pa1 * pow_x / (pb1 * pb2 * fact);
-
-                // Update all running variables
-                pa1 *= a1 + k;
-                pb1 *= b1 + k;
-                pb2 *= b2 + k;
-
-                fact *= k + 1;
-                pow_x *= x;
+                // Based on the definition of 1F2 each term differs from the previous by multiplicative factors that have to do with the Pochhammer symbols, power of x and the factorial in the denominator
+                term *= (a1 + k) * x / ((b1 + k) * (b2 + k) * (k + 1));
         }
 
         return result;
@@ -46,36 +31,14 @@ double hyp2F3(const double a1, const double a2, const double b1, const double b2
 {
         int k;
 
-        // Define variables that will contain the running value of the Pochhammer symbols corresponding to the a_i and b_i
-        double pa1 = 1;         // (a1)_0 = 1 by the definition of the Pochhammer symbol
-        double pa2 = 1;
-        double pb1 = 1;
-        double pb2 = 1;
-        double pb3 = 1;
-
-        // Define running variables for power of x and factorial
-        double fact = 1;
-        double pow_x = 1;
-
         double result = 0;
-        double prev = -1;
+        double term = 1;
 
-
-        for (k = 0; (k < MAX_TERMS) & (fabs(result - prev) > TOLERANCE); ++k)
+        for (k = 0; (k < MAX_TERMS) & (fabs(term) > TOLERANCE); ++k)
         {
-                prev = result;
+                result += term;
 
-                result += pa1 * pa2 * pow_x / (pb1 * pb2 * pb3 * fact);
-
-                // Update all running variables
-                pa1 *= a1 + k;
-                pa2 *= a2 + k;
-                pb1 *= b1 + k;
-                pb2 *= b2 + k;
-                pb3 *= b3 + k;
-
-                fact *= k + 1;
-                pow_x *= x;
+                term *= (a1 + k) * (a2 + k) * x / ((b1 + k) * (b2 + k) * (b3 + k) * (k + 1));
         }
 
         return result;
