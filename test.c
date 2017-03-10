346251d432367753c92098896b74aad505ffbbf9
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-03-10 11:44:46 +0500

    Print out values of the specie_h function

--- test.c
+++ test.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include "constants.h"
 #include "functions.h"
 
 
 int main(void)
 {
-        printf("\nRHO_C = %.20f", RHO_C);
-        printf("\nRHO_H = %d", RHO_H);
-        printf("\nLAMBDA_KAPPA_C_p2 = %.20f", LAMBDA_KAPPA_C_p2);
-        printf("\nLAMBDA_KAPPA_H_p2 = %.20f", LAMBDA_KAPPA_H_p2);
-        printf("\n\nspecie_c(1.5, 1.5) = %.20f", specie_c(1, 1.5));
-        printf("\n\nspecie_h(1.5, 1.5) = %.20f", specie_h(1, 1.5));
+        int i;
+
+        for (i = 0; i < 20; ++i)
+                printf("\nspecie_h(%.2f, 1.5) = %.20f", i / 5.0, specie_h(i / 5.0, i / 5.0));
 
         printf("\n\n");
 
