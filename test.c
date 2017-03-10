dc5dbaee3e4d14d1bd9a9013c086fc27c78fdcaf
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-03-10 12:19:22 +0500

    Checking value of specie_h at k_perp = 0

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "functions.h"
 
 
@@ -6,9 +7,16 @@ int main(void)
 {
         int i;
 
-        for (i = 0; i < 20; ++i)
-                printf("\nspecie_h(%.2f, 1.5) = %.20f", i / 5.0, specie_h(i / 5.0, i / 5.0));
+        // for (i = 0; i < 20; ++i)
+        //         printf("\nspecie_h(%.2f, 1.5) = %.20f", i / 5.0, specie_h(i / 5.0, 1.5));
 
+        for (i = 0; i < 20; ++i)
+        {
+                double k = pow(10, -i);
+                printf("\nspecie(%.1e, 1.5) = %.10e", k, specie_h(k, 1.5));
+        }
+
+        // printf("\nspecie_h(0, 1.5) = %.20f", specie_h(1e-9, 1.5));
         printf("\n\n");
 
         return 0;
