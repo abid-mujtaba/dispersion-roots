e9c0754f080475486fff789728dce0eb42c42a15
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-03-12 13:34:55 +0500

    Change C test to aid in debugging.

--- test.c
+++ test.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 #include "functions.h"
+#include "hypergeom.h"
 
 
 int main(void)
 {
-        int i;
+        // int i;
+        //
+        // for (i = 0; i < 100; ++i)
+        // {
+        //         double k = i / 5.0;
+        //
+        //         printf("\nD(%.2f, 0.5) = %.5e", k, D(k, 0.5));
+        // }
+        //
+        // printf("\n\n");
+        //
+        // for (i = 0; i < 20; ++i)
+        // {
+        //         double w = i / 20.0;
+        //
+        //         printf("\nD(15, %.2f) = %.5e", w, D(15, w));
+        // }
 
-        for (i = 0; i < 20; ++i)
-        {
-                double k = i / 5.0;
+        // printf("\n2F3(1, 0.5, -3.5, 1.5, 0.5, 231.2) = %.20f", hyp2F3(1, 0.5, -3.5, 1.5, 0.5, 231.2));
+        // printf("\n1F2(5, 6, 5, 231.2) = %.20f", hyp1F2(5, 6, 5, 231.2));
 
-                printf("\nD(%.2f, 1.5) = %.5e", k, D(k, 1.5));
-        }
-
-        printf("\n\n");
-
-        for (i = 0; i < 20; ++i)
-        {
-                double w = i / 5.0;
-
-                printf("\nD(1.5, %.2f) = %.5e", w, D(1.5, w));
-        }
+        double k = 6.8, w = 0.5;
+        printf("\n\nD(%.1f, %.1f) = %.20f", k, w, D(k, w));
 
         printf("\n\n");
 
