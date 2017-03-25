6dd2428bad64954ede62d0dbca33ef1034846526
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-03-25 23:25:26 +0500

    Print 2F3 value for testing

--- test.c
+++ test.c
@@ -37,9 +37,11 @@ int main(void)
         double omega_by_omega_cj = omega / OMEGA_CH;
         double two_lambda_H_prime = calc_two_lambda_j_prime(KAPPA_H, RHO_H, k_perp);
         struct coeffs_1f2 c_1f2 = calc_coeffs_1f2(KAPPA_H, omega_by_omega_cj);
+        struct coeffs_2f3 c_2f3 = calc_coeffs_2f3(KAPPA_H, omega_by_omega_cj);
 
         printf("\n\ncoeff = %f", calc_coeff(omega_by_omega_cj, KAPPA_H, two_lambda_H_prime));
         printf("\n1F2(k_perp = %.1f, omega = %.1f) = %.17g", k_perp, omega, hyp1F2(c_1f2, two_lambda_H_prime));
+        printf("\n2F3(k_perp = %.1f, omega = %.1f) = %.17g", k_perp, omega, hyp2F3(c_2f3, two_lambda_H_prime));
 
 
         // mpfr_set_default_prec(PRECISION);               // Set default precision for all variables whose precision is NOT explicitly specified when initialized
