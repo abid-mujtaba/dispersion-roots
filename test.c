f9bd4afdecede8c642307441cebe2cdf2ac14d07
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-03-30 10:44:53 +0500

    Testing D at k = 0

--- test.c
+++ test.c
@@ -5,18 +5,33 @@
 
 int main(void)
 {
-        double slices[10];
-        double omegas[10];
-        double roots[10];
-        int NUM = 7;
+        // double slices[10];
+        // double omegas[10];
+        // double roots[10];
+        // int NUM = 7;
+        //
+        // for (int j = 0; j < NUM; ++j)
+        //         slices[j] = j + 1.5;
+        //
+        // int N = find_k_perp_roots_array(slices, omegas, roots, NUM);
+        //
+        // for (int i = 0; i < N; ++i)
+        //         printf("\nRoot at omega = %.2f is %f", omegas[i], roots[i]);
 
-        for (int j = 0; j < NUM; ++j)
-                slices[j] = j + 1.5;
+        // double k = 0;
+        // double omega = 5 + 1e-20;
+        //
+        // printf("\nD(%.2f, %.2f) = %f\n", k, omega, D(k, omega));
+        //
+        // k = 1e-20;
+        // printf("\nD(%.2f, %.2f) = %f", k, omega, D(k, omega));
 
-        int N = find_k_perp_roots_array(slices, omegas, roots, NUM);
+        double k = 1e-20;
 
-        for (int i = 0; i < N; ++i)
-                printf("\nRoot at omega = %.2f is %f", omegas[i], roots[i]);
+        double omega = 5 + 1e-10;
+
+        for (int i = 0; i < 10; ++i)
+                printf("\nD(%.2f, %.2f) = %f", k, omega + i * 0.1, D(k, omega + i * 0.1));
 
         printf("\n\n");
 
