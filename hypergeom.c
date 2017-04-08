5eaddc8405f35cb0182827f0831c05a697ed8bb1
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-04-09 00:28:26 +0500

    Deal with non-positive integer values for c.b2

--- hypergeom.c
+++ hypergeom.c
@@ -115,7 +115,18 @@ void norm_hyp1F2(mpfr_t result, const struct coeffs_1f2 c, const mpfr_t x)
         mpfr_set_d(term, 1, RND);         // Stores the running value of each term in the summation. From the definition of the Pochhammer symbols the value of the k = 0 terms is ONE and then we divide it by the gamma functions
         mpfr_gamma(v, c.b1, RND);
         mpfr_div(term, term, v, RND);
-        mpfr_gamma(v, c.b2, RND);
+
+        // We check if c.b2 is possibly a negative integer (or zero)
+        double b2 = mpfr_get_d(c.b2, RND);
+        int start = 0;
+
+        if ((b2 <= 0) & (b2 == (int) b2))
+                start = (-b2) + 1;              // In this case the first few terms will have to be neglected since they will be divided by the Gamma of a non-positive integer which is infinity and so upon division these terms will become zero
+
+
+        // We add 'start' to c.b2. If it is zero, no harm. If it is not zero then c.b2 + start = 1 and Gamma(1) = 1 so still no harm
+        mpfr_add_d(v, c.b2, start, RND);
+        mpfr_gamma(v, v, RND);
         mpfr_div(term, term, v, RND);
 
         mpfr_abs(fterm, term, RND);
@@ -125,7 +136,9 @@ void norm_hyp1F2(mpfr_t result, const struct coeffs_1f2 c, const mpfr_t x)
 
         for (k = 0; (k < MAX_TERMS) & (mpfr_cmp_d(fterm, TOLERANCE) > 0); ++k)
         {
-                mpfr_add(result, result, term, RND);
+                // for start != 0 the first 'start' terms are to be neglected so we don't add them to the result
+                if (k >= start)
+                        mpfr_add(result, result, term, RND);
 
                 mpfr_add_d(v, c.a1, k, RND);
                 mpfr_mul(term, term, v, RND);
@@ -134,8 +147,11 @@ void norm_hyp1F2(mpfr_t result, const struct coeffs_1f2 c, const mpfr_t x)
                 mpfr_add_d(v, c.b1, k, RND);
                 mpfr_div(term, term, v, RND);
 
-                mpfr_add_d(v, c.b2, k, RND);
-                mpfr_div(term, term, v, RND);
+                if (k >= start)         // For start != 0 Gamma(c.b2 + k) = infinity and so the term is neglected
+                {
+                        mpfr_add_d(v, c.b2, k, RND);
+                        mpfr_div(term, term, v, RND);
+                }
 
                 mpfr_mul(term, term, x, RND);
                 mpfr_div_d(term, term, k + 1, RND);
