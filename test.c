0644a6f8a215cbde3489e17fd09f35418e7e1abc
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-05-10 00:05:48 +0500

    Restore multi-value test

--- test.c
+++ test.c
@@ -9,35 +9,26 @@ double lambda_vcj_p2(double, double, double);
 
 int main(void)
 {
-        // double k_perp = 0.1;
-        // double omega = 0.6;
-        //
-        // printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-        //
-        // omega = 1.6;
-        // printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-        //
-        // omega = 2.5;
-        // printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-        //
-        // k_perp = 1.4;
-        // omega = 3.6;
-        // printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-        //
-        // omega = 4.6;
-        // printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-        //
-        // omega = 5.5;
-        // printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
+        double k_perp = 0.1;
+        double omega = 0.6;
 
+        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
 
-        double l = lambda_vcj_p2(KAPPA_H, RHO_H, N0H_BY_N0E);
-        double denom = (KAPPA_H - 1.5) * (3 * LAMBDA + 1) * (KAPPA_H + 1) * (KAPPA_H + 0.5) * gsl_sf_gamma(KAPPA_H - 0.5);
-        double b2 = denom / l;
+        omega = 1.6;
+        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
 
-        printf("\nlambda_vcj_p2 = %.17g", l);
-        printf("\ndenom = %.17g", denom);
-        printf("\nbeta^2 = %.17g", b2);
+        omega = 2.5;
+        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
+
+        k_perp = 1.4;
+        omega = 3.6;
+        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
+
+        omega = 4.6;
+        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
+
+        omega = 5.5;
+        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
 
 
         printf("\n\n");
