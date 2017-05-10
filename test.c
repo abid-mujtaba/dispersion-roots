75410037802a08ae0f14c43313d7089b3dd6e013
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-05-10 17:18:16 +0500

    Test for k_perp = 0

--- test.c
+++ test.c
@@ -4,32 +4,12 @@
 #include <gsl/gsl_sf_gamma.h>
 
 
-double lambda_vcj_p2(double, double, double);
-
-
 int main(void)
 {
-        double k_perp = 0.1;
-        double omega = 0.6;
+        double k_perp = 0.0;
+        double omega = 1.6;
 
         printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-
-        omega = 1.6;
-        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-
-        omega = 2.5;
-        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-
-        k_perp = 1.4;
-        omega = 3.6;
-        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-
-        omega = 4.6;
-        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-
-        omega = 5.5;
-        printf("\nD(%.1f, %.1f) = %.17g", k_perp, omega, D(k_perp, omega));
-
 
         printf("\n\n");
 
