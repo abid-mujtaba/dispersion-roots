57535a07eefa3548feaa63fbe300d428bbcd3661
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-05-15 00:31:52 +0500

    Convert mpfr variable to string

--- test.c
+++ test.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include "roots.h"
+#include "constants.h"
+#include <mpfr.h>
+
+
+void test1();
 
 
 int main(void)
+{
+        // test1();
+        mpfr_t x;
+        mpfr_init(x);
+
+        mpfr_set_d(x, 7.42, RND);
+        mpfr_gamma(x, x, RND);
+        mpfr_printf("\nx = %.17RG", x);
+
+        char * res;
+        mpfr_exp_t * expptr;
+
+        res = mpfr_get_str((char *) 0, expptr, 10, 0, x, RND);
+        printf("\nstring = %s", res);
+        mpfr_free_str(res);
+
+        mpfr_clear(x);
+
+
+        printf("\n\n");
+
+        return 0;
+}
+
+
+void test1()
 {
         const int size = 20;
         const int initial = 6;
@@ -14,8 +45,4 @@ int main(void)
 
         for (int i = 0; i < num; ++i)
                 printf("\nD root at k_perp = %.2f  -> %.17g", k_perps[i], omegas[i]);
-
-        printf("\n\n");
-
-        return 0;
 }
