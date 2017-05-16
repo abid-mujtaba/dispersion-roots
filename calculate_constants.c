8762913a545f10330d605bf241cdfecef98817ee
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-05-16 22:49:46 +0500

    Calculate kappa before hand and recycle

--- calculate_constants.c
+++ calculate_constants.c
@@ -13,7 +13,7 @@
 #include <mpfr.h>
 
 
-void calc_lambda_vcj_p2(mpfr_t result, double kappa, double rho, double n0_by_n0e, mpfr_t k, mpfr_t x);
+void calc_lambda_vcj_p2(mpfr_t result, mpfr_t kappa, double rho, double n0_by_n0e, mpfr_t x);
 
 
 int main(void)
@@ -33,12 +33,19 @@ int main(void)
     fprintf(fout, "\n#define N0C_BY_N0E %.17g", n0c_by_n0e);
 
 
-    calc_lambda_vcj_p2(res, KAPPA_H, RHO_H, N0H_BY_N0E, kappa, x);
+    // Hot specie calculations
+    mpfr_set_d(kappa, KAPPA_H, RND);
+
+    calc_lambda_vcj_p2(res, kappa, RHO_H, N0H_BY_N0E, x);
     fprintf(fout, "\n#define LAMBDA_VCH_P2 \"");
     mpfr_out_str(fout, 10, 0, res, RND);
     fprintf(fout, "\"");
 
-    calc_lambda_vcj_p2(res, KAPPA_C, rho_c, n0c_by_n0e, kappa, x);
+
+    // Cold specie calculations
+    mpfr_set_d(kappa, KAPPA_C, RND);
+    
+    calc_lambda_vcj_p2(res, kappa, rho_c, n0c_by_n0e, x);
     fprintf(fout, "\n#define LAMBDA_VCC_P2 \"");
     mpfr_out_str(fout, 10, 0, res, RND);
     fprintf(fout, "\"");
@@ -52,25 +59,23 @@ int main(void)
 }
 
 
-void calc_lambda_vcj_p2(mpfr_t res, double kappa, double rho, double n0_by_n0e, mpfr_t k, mpfr_t x)
+void calc_lambda_vcj_p2(mpfr_t res, mpfr_t kappa, double rho, double n0_by_n0e, mpfr_t x)
 {
-    mpfr_set_d(k, kappa, RND);
-
     mpfr_set_d(res, 3 * LAMBDA + 1, RND);        // r = 3 * LAMBDA + 1
 
-    mpfr_add_ui(x, k, 1, RND);
+    mpfr_add_ui(x, kappa, 1, RND);
     mpfr_mul(res, res, x, RND);                  // r *= (kappa + 1)
 
-    mpfr_add_d(x, k, 1.5, RND);
+    mpfr_add_d(x, kappa, 1.5, RND);
     mpfr_gamma(x, x, RND);
     mpfr_mul(res, res, x, RND);                 // r *= gamma(kappa + 1.5)
 
-    mpfr_sub_d(x, k, 1.5, RND);
+    mpfr_sub_d(x, kappa, 1.5, RND);
     mpfr_mul(res, res, x, RND);                 // r *= (kappa - 1.5)
     mpfr_mul_d(res, res, pow(rho, 2), RND);     // r *= pow(rho_j, 2)
     mpfr_div_d(res, res, n0_by_n0e, RND);       // r /= n0_by_n0e
     mpfr_div_d(res, res, pow(OMEGA_UH_BY_OMEGA_CE, 2) - 1, RND);    // r /= (pow(OMEGA_UH_BY_OMEGA_CE, 2)  - 1)
 
-    mpfr_sub_d(x, k, 0.5, RND);
+    mpfr_sub_d(x, kappa, 0.5, RND);
     mpfr_div(res, res, x, RND);                 // r =/ (kappa - 0.5)
 }
