69fc9011dc059b44d29795641b8d32bf242d422e
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-07-24 15:20:03 +0500

    Add PLOT label and change value of LAMBDA

--- constants.h
+++ constants.h
@@ -4,6 +4,9 @@
 
  #include <mpfr.h>
 
+// Define the number and version of the plot whose data is being calculated
+#define PLOT "01-1-a"
+
 
 // Set to 1 if you want debug information printed
 #define DEBUG 1
@@ -20,7 +23,7 @@
 // We use macros to define all variables whose values we would need to adjust. The rest are derived from these.
 #define KAPPA_C 2
 #define KAPPA_H 4
-#define LAMBDA 0.2          // Capital Lambda used in VC Dispersion
+#define LAMBDA 0.05          // Capital Lambda used in VC Dispersion
 
 #define N0H_BY_N0E 1.0    // Relative density of hot electrons out of total (hot and cold) electrons)
 
