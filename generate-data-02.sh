17872caa82813b4d1115ddf170065a1993205bbe
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-07-28 14:57:43 +0500

    Create shell script to generate series of data by using sed on constants.h

---
+++ generate-data-02.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Script that is used to generate series of data for the first plot
+# by modifying constants.h and running the program repeatedly
+
+# First we make the global changes (values that are constant across the entire series)
+sed -i constants.h -e 's/LAMBDA.*$/LAMBDA 0.15/' -e 's/KAPPA_C.*$/KAPPA_C 2.0/' -e 's/N0H_BY_N0E.*$/N0H_BY_N0E 0.5/'
+
+
+# Now we modify the value of KAPPA_H and generate the corresponding data
+sed -i constants.h -e 's/PLOT.*$/PLOT "02-a"/' -e 's/KAPPA_H.*$/KAPPA_H 1.6/'
+make data
+
+sed -i constants.h -e 's/PLOT.*$/PLOT "02-b"/' -e 's/KAPPA_H.*$/KAPPA_H 2.0/'
+make data
+
+
+# Beep three times to indicate end of the calculation
+sudo ~/bin/beeps
