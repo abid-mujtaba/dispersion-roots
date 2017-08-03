7fcf6f09b8de81e97571c3378b0258db7ff50bba
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-08-03 13:46:59 +0500

    Remove old k_perp root finding code using gsl

--- roots.h
+++ roots.h
@@ -3,19 +3,10 @@
  */
 
 // Define the bracket limits for root finding
-#define ROOT_LO 0           // Function is nan at zero so we avoid it
-#define ROOT_HI 100
 #define K_PERP_MAX 100          // Max value of k_perp for root finding
 #define ROOT_INTERVAL 1e-3
-#define ROOT_MAX_ITERATIONS 13          // to get to 1e-3 from an initial interval of 5 takes about 13 binary divisions
+#define DELTA 1e-10             // Minimum value to move away from thresholds (e.g. k_perp = 0 + DELTA since the dispersion relation is undefined at k_perp = 0)
 
-// Define the threshold interval for finding the root. Once the bracket becomes
-// smaller than this value the root-finding iteration will stop
-#define ROOT_BRACKET 1e-4
-
-
-double find_k_perp_root(const double omega, const double lo, const double hi);
-int find_k_perp_roots_array(double slices[], double omega[], double roots[], int size);
 
 int find_omega_root(const double k_perp, const double lo, const double hi, double * root, const double guess);
 int find_omega_roots_array(const int initial, double k_perp_samples[], double k_perps[], double omegas[], const int size);
