b7590266af36a15471a6b43adc266020697f5e44
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-08-24 17:19:06 +0500

    Implement skeletons of second and third parts of term calculation.

--- term.c
+++ term.c
@@ -8,7 +8,8 @@
 #include "math_utilities.h"
 
 
-void calculate_term(mpfr_t r, int n, struct Constants * const c, mpfr_t x, mpfr_t y, mpfr_t * vars);
+void second(mpfr_t r, int n, struct Constants * const c);
+void third(mpfr_t r, int n, struct Constants * const c, mpfr_t x, mpfr_t y, mpfr_t * vars);
 
 
 void term(mpfr_t res, int n, struct Constants * const c, mpfr_t * const vars)
@@ -20,17 +21,43 @@ void term(mpfr_t res, int n, struct Constants * const c, mpfr_t * const vars)
         return;
     }
 
-    calculate_term(res, n, c, * vars, * (vars + 1), vars + 2);
+    mpfr_t * x = vars;
+
+    // res = alpha[n] * (1 - 2F3 - third)
+    mpfr_set_ui(res, 1, RND);
+
+    second(*x, n, c);
+    mpfr_sub(res, res, *x, RND);         // res = 1 - 2F3
+
+    third(*x, n, c, * (vars + 1), * (vars + 2), vars + 3);
+    mpfr_sub(res, res, *x, RND);         // res -= third
+
+    alpha(*x, n, LAMBDA, c->kappa, * vars, * (vars + 1));
+    mpfr_mul(res, res, *x, RND);         // res *= alpha[n]
 }
 
 
-void calculate_term(mpfr_t r, int n, struct Constants * const c, mpfr_t x, mpfr_t y, mpfr_t * vars)
+/*
+        2F3[1/2, n; n - 1/2 -k, 1 - w/w_cj, 1 + w/w_cj; 2 lambda_j]
+*/
+void second(mpfr_t r, int n, struct Constants * const c)
 {
-    alpha(r, n, LAMBDA, c->kappa, * vars, * (vars + 1));    // r = alpha[n]
+    // TODO: Implement
+    mpfr_set_ui(r, 1, RND);
+}
 
+
+/*
+        third = (k + 1/2)_-n / (n-1)! * sqrt(pi) csc(pi * w/w_cj)
+*/
+void third(mpfr_t r, int n, struct Constants * const c, mpfr_t x, mpfr_t y, mpfr_t * vars)
+{
     mpfr_add_d(x, c->kappa, 0.5, RND);
     neg_pochammer(y, n, x, * vars);
     mpfr_mul(r, r, y, RND);                            // r *= (k + 1/2)_-n
 
     mpfr_div_ui(r, r, factorial(n - 1), RND);          // r /= (n - 1)!
+
+    mpfr_mul(r, r, c->sqrt_pi, RND);            // r *= sqrt(pi)
+    mpfr_mul(r, r, c->csc, RND);                // r *= csc(pi * w_j / w_ce)
 }
