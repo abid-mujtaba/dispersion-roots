ac3c7e7251a14e1b53dddcd79bb2686cabf24e49
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-08-24 17:58:52 +0500

    Second part of term implemented.

--- term.c
+++ term.c
@@ -9,8 +9,9 @@
 #include "math_utilities.h"
 
 
-void second(mpfr_t r, int n, struct Constants * const c);
+void second(mpfr_t r, int n, struct Constants * const c, mpfr_t * vars);
 void third(mpfr_t r, int n, struct Constants * const c, mpfr_t x, mpfr_t y, mpfr_t * vars);
+void calc_second_coeffs_2f3(struct coeffs_2f3 * const c, int n, const mpfr_t k, const mpfr_t w, mpfr_t * vars);
 void calc_third_coeffs_2f3(struct coeffs_2f3 * const c, int n, const mpfr_t k, const mpfr_t w, mpfr_t * vars);
 
 
@@ -28,7 +29,7 @@ void term(mpfr_t res, int n, struct Constants * const c, mpfr_t * const vars)
     // res = alpha[n] * (1 - 2F3 - third)
     mpfr_set_ui(res, 1, RND);
 
-    second(*x, n, c);
+    second(*x, n, c, vars + 1);
     mpfr_sub(res, res, *x, RND);         // res = 1 - 2F3
 
     third(*x, n, c, * (vars + 1), * (vars + 2), vars + 3);
@@ -42,12 +43,37 @@ void term(mpfr_t res, int n, struct Constants * const c, mpfr_t * const vars)
 /*
         2F3[1/2, n; n - 1/2 -k, 1 - w/w_cj, 1 + w/w_cj; 2 lambda_j]
 */
-void second(mpfr_t r, int n, struct Constants * const c)
+void second(mpfr_t r, int n, struct Constants * const c, mpfr_t * vars)
 {
-    // TODO: Implement
-    mpfr_set_ui(r, 1, RND);
+    struct coeffs_2f3 c_2f3;
+    calc_second_coeffs_2f3(& c_2f3, n, c->kappa, c->omega_by_omega_c, vars);
+
+    norm_hyp2F3(r, c_2f3, c->two_lambda);
 }
 
+/*
+        2F3[1/2, n; n - 1/2 -k, 1 - w/w_cj, 1 + w/w_cj; 2 lambda_j]
+*/
+void calc_second_coeffs_2f3(struct coeffs_2f3 * const c, int n, const mpfr_t k, const mpfr_t w, mpfr_t * vars)
+{
+    c->a1 = vars;
+    c->a2 = vars + 1;
+    c->b1 = vars + 2;
+    c->b2 = vars + 3;
+    c->b3 = vars + 4;
+
+    mpfr_set_d(* c->a1, 0.5, RND);          // a1 = 1/2
+    mpfr_set_ui(* c->a2, n, RND);           // a2 = n
+
+    mpfr_sub_d(* c->b1, c->a2, 0.5, RND);
+    mpfr_sub(* c->b1, * c->b1, k, RND);         // b1 = n - 1/2 - k
+
+    mpfr_set_ui(* c->b2, 1, RND);
+    mpfr_sub(* c->b2, * c->b2, w, RND);         // b2 = 1 - w
+
+    mpfr_set_ui(* c->b3, 1, RND);
+    mpfr_add(* c->b3, * c->b3, w, RND);         // b3 = 1 + w
+}
 
 /*
         third = (k + 1/2)_-n / (n-1)! * sqrt(pi) csc(pi * w/w_cj) (2 lambda_j)^(k + 3/2 - n) Gamma(k + 2 - n) Gamma(n - 3/2 - k)
