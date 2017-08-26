e192452685a40954e074d6c1171517da5a8a0a1f
Abid H Mujtaba <abid.naqvi83@gmail.com>
2017-08-26 17:57:03 +0500

    Revert last kappa_h to inf

--- generate_data_02.py
+++ generate_data_02.py
@@ -8,7 +8,7 @@ from generate_data_base import set_defaults, set_roots_value, iterate_variable
 
 PLOT = "02"
 VARIABLE = 'KAPPA_H'
-VALUES = [1.6, 2.0, 10.0]
+VALUES = [1.6, 2.0, 'INFINITY']
 
 # We first declare the default values we want to set as a dictionary
 defaults = {
