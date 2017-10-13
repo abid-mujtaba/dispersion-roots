d05375125c3cdf0b74bb14a6cbb04cbcc4e7f112
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-10-13 14:48:02 +0500

    Modify plot 4

--- generate_data_04.py
+++ generate_data_04.py
@@ -1,29 +1,27 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 #
 # Script for generating data for plot 02 (different values of KAPPA_H)
 
 
-from generate_data_base import set_defaults, set_roots_value, iterate_variables
+from generate_data_base import set_defaults, set_roots_value, set_data_value, iterate_variables
 
 
 PLOT = "04"
 VARIABLES = {
-            'KAPPA_H': [1.6, 2.0, 'INFINITY'],
-            'MAX_TERMS': [1000, 1000, 100000],      # The case of kappa_h = inf requires more terms and higher precision
-            'MIN_PRECISION': [128, 128, 256],
-            'DOUBLE_PRECISION_DELTA': [30, 30, 7],
-    }
+    'LAMBDA_C': [0.00, 0.01, 0.1, 0.25],
+}
+
 
 # We first declare the default values we want to set as a dictionary
 defaults = {
-        'LAMBDA_C': 0.2,
-        'LAMBDA_H': 0.2,
+        'LAMBDA_H': 0.01,
         'KAPPA_C': 2.0,
+        'KAPPA_H': 4.0,
         'N0H_BY_N0E': 0.5,
         'TH_BY_TC': 101.695
     }
 
-K_PERP_MAX = 10
+K_PERP_MAX = 20
 
 
 
