b10da8e7f4c4b72d6493c72f1641e77c9a08e91c
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-10-16 15:01:17 +0500

    Modify plot 7

--- data/plot-07.R
+++ data/plot-07.R
@@ -1,13 +1,19 @@
 library(rjson)
+library(tikzDevice)
 library(ggplot2)
 
-p <- ggplot()       # Initiate empty plot
+
+# Open the tikz device for output
+tikz(file='plot-07.tex', width=7, height=7)
+
+p <- ggplot() + theme_bw()       # Initiate empty plot
 
 
 # We have several series of data so we define a function that takes the file index (name/label) as well the linetype to use for that set and appends the appropriate plots
-subplot <- function(p, index, lambda) {
+subplot <- function(p, index, parameter) {
 
-    # Read data from csv files:
+    # Read data from csv file:
+
     DATAFILE <- paste("data-", index, ".csv", sep="")
     s <- read.csv(DATAFILE)
 
@@ -15,32 +21,38 @@ subplot <- function(p, index, lambda) {
     # Create sub-plots for the sequences
     for (seq in 1:7) {
         ss <- s[s$seq == seq,]      # Create sub-set of the data for specified value of 'seq'
-        p <- p + geom_line(data=ss, aes_(x=ss$k_perp, y=ss$omega, linetype=lambda))            # Use 'aes_' to gain access to local variable kappa_h (scope problems). This requires x= and y= to be declared explicitly. We set 'linetype' equal to the 'kappa_h' value and later manually provide a conversion from kappa_h value to the linetype
+        p <- p + geom_line(data=ss, aes_(x=ss$k_perp, y=ss$omega, linetype=parameter))            # Use 'aes_' to gain access to local variable kappa_h (scope problems). This requires x= and y= to be declared explicitly. We set 'linetype' equal to the 'parameter' value and later manually provide a conversion from parameter value to the linetype
     }
 
 
-    # Add title to plot. Note use of expression() to access the plotmath ability to typecast equations/sybmols
-    # and the use of paste() to combine normal text and symbols
-    p <- p +
-            labs(x = expression(k[perp]), y = expression(omega / omega[ce]))
+    # Add title to plot. Note use of LaTeX expressions which are quoted and the back-slash is escaped
+    # The .tex output will contain these LaTeX expressions inside the tikz diagram and they will be correctly rendered by pdflatex
 
     return (p)
 }
 
 
 # Repeatedly call subplot to add series of data
-p <- subplot(p, "07-a", "0.00")
-p <- subplot(p, "07-b", "0.01")
-p <- subplot(p, "07-c", "0.10")
-p <- subplot(p, "07-d", "0.25")
-p <- p + scale_linetype_manual(name=expression(Lambda), values=c("0.00"="solid", "0.01"="dotted", "0.10"="dashed", "0.25"="dotdash")) +      # The 'name' will be the title of the legend
-         ggtitle(expression(paste("Roots of Dispersion Relation for ", kappa[c], " = 2, ", kappa[h], " = 4, ", frac(n[h0], n[e0]), " = 0.5")))
+p <- subplot(p, "07-a", "0.0")
+p <- subplot(p, "07-b", "0.3")
+p <- subplot(p, "07-c", "0.5")
+p <- subplot(p, "07-d", "0.8")
+p <- subplot(p, "07-e", "1.0")
 
-#p <- p + xlim(0,10)        # Limit x-axis values
+p <- p + labs(x = "$k_\\perp$", y = "$\\displaystyle \\frac{\\omega}{\\omega_{ce}}$") +
+         ggtitle("Dispersion for $\\Lambda_c = 0.01$, $\\Lambda_h = 0.1$, $\\kappa_c = 2$, $\\kappa_h = 4$, $\\frac{T_h}{T_c} = 101.695$")
+
+p <- p + scale_linetype_manual(name="$\\displaystyle \\frac{n_{0h}}{n_{0e}}$", values=c("0.0"="solid", "0.3"="dashed", "0.5"="dotted", "0.8"="dotdash", "1.0"="longdash"))
+
+# Rotate the y-axis title and make its vertical justification centered
+p <- p + theme(axis.title.y = element_text(angle = 0, vjust=0.5))
+
+axis.title = element_text(size = rel(1.5))      # Increase size of axis titles
 
 
-ggsave(file="plot-07.pdf", plot=p)
+print(p)        # This will create the output using tikzDevice
+dev.off()       # Necessary to close tikzDevice so the .tex file is written
 
 
 # Source: http://www.cookbook-r.com/Graphs/Shapes_and_line_types/
-# Source for math expressions: http://vis.supstat.com/2013/04/mathematical-annotation-in-r/
+# Source for tikzdevice: http://iltabiai.github.io/tips/latex/2015/09/15/latex-tikzdevice-r.html
--- generate_data_07.py
+++ generate_data_07.py
@@ -8,16 +8,16 @@ from generate_data_base import set_defaults, set_roots_value, iterate_variables
 
 PLOT = "07"
 VARIABLES = {
-    'LAMBDA_C': [0.00, 0.01, 0.10, 0.25],
-    'LAMBDA_H': [0.00, 0.01, 0.10, 0.25],
+        'N0H_BY_N0E': [0.0, 0.3, 0.5, 0.8, 1.0],
 }
 
 
 # We first declare the default values we want to set as a dictionary
 defaults = {
+        'LAMBDA_C': 0.01,
+        'LAMBDA_H': 0.1,
         'KAPPA_C': 2.0,
         'KAPPA_H': 4.0,
-        'N0H_BY_N0E': 0.5,
         'TH_BY_TC': 101.695
     }
 
