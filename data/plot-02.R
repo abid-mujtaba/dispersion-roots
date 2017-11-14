27e8b916e1065ff9ed511c3792b5d965e27b4d73
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-11-14 15:01:02 +0500

    Fix typo in legend.

--- data/plot-02.R
+++ data/plot-02.R
@@ -14,7 +14,7 @@ p <- p + ggtitle("Dispersion for $\\Lambda_h = 0$, $\\kappa_c = 2$, $\\kappa_h =
 p <- p +
         scale_x_continuous("$k_\\perp$", sec.axis = custom_secondary_axis, expand  = c(0,0)) +
         scale_y_continuous("$\\displaystyle \\frac{\\omega}{\\omega_{ce}}$", sec.axis = custom_secondary_axis) +
-        scale_linetype_manual(name="$\\Lambda_h$", values=c("0.00"="solid", "0.01"="dotted", "0.10"="2282", "0.25"="dashed")) +
+        scale_linetype_manual(name="$\\Lambda_c$", values=c("0.00"="solid", "0.01"="dotted", "0.10"="2282", "0.25"="dashed")) +
         expand_limits(x = 0) +    # Make graph start at x = 0 (snug up with left margin)
         custom_theme
 
