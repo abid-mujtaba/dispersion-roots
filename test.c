58910ed530e6571438450c29ee6f3903d7a7a6b4
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-11-21 15:30:19 +0500

    Add test2 for k_perp = 0 investigation.

--- test.c
+++ test.c
@@ -9,39 +9,55 @@
 #define SIZE 1
 
 
+void constants();
 void test1();
+void test2();
 
 
 int main(void)
 {
-        test1();
+    test2();
 
-        printf("\n\n");
+    printf("\n\n");
 
-        return 0;
+    return 0;
+}
+
+
+void constants()
+{
+    printf("\nKAPPA_C = %.17g", KAPPA_C);
+    printf("\nKAPPA_H = %.17g", KAPPA_H);
+    printf("\nLAMBDA_C = %.17g", LAMBDA_C);
+    printf("\nLAMBDA_H = %.17g", LAMBDA_H);
+    printf("\nN0H_BY_N0E = %.17g", N0H_BY_N0E);
+    printf("\nTH_BY_TC = %.17g\n", TH_BY_TC);
 }
 
 
 void test1()
 {
-        printf("\nKAPPA_C = %.17g", KAPPA_C);
-        printf("\nKAPPA_H = %.17g", KAPPA_H);
-        printf("\nLAMBDA_C = %.17g", LAMBDA_C);
-        printf("\nLAMBDA_H = %.17g", LAMBDA_H);
-        printf("\nN0H_BY_N0E = %.17g", N0H_BY_N0E);
-        printf("\nTH_BY_TC = %.17g\n", TH_BY_TC);
+    constants();
 
-        // Print samples of Disperstion function.
-        for (double w0 = 1; w0 < 8; ++w0)
-        {
-                for (double dw = 0.1; dw < 1; dw += 0.4)
-                {
-                        double w = w0 + dw;
+    // Print samples of Disperstion function.
+    for (double w0 = 1; w0 < 8; ++w0)
+    {
+            for (double dw = 0.1; dw < 1; dw += 0.4)
+            {
+                    double w = w0 + dw;
 
-                        for (int k = 0; k < 40; k += 5)
-                        {
-                                printf("\nD(%2d, %.1f) = %.17g", k, w, D(k,w));
-                        }
-                }
-        }
+                    for (int k = 0; k < 40; k += 5)
+                    {
+                            printf("\nD(%2d, %.1f) = %.17g", k, w, D(k,w));
+                    }
+            }
+    }
+}
+
+
+void test2()
+{
+    const double w = 1.1;
+
+    printf("\nD(0, %.1f) = %.17g", w, D(0,w));
 }
