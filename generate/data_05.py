193264ae11fa93a6b0858ce5caca930967ec02f9
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-11-22 15:39:58 +0500

    Make kappa values explicitly float.

--- generate/data_05.py
+++ generate/data_05.py
@@ -8,7 +8,7 @@ from data_base import set_defaults, set_roots_value, iterate_variables
 
 PLOT = "05"
 VARIABLES = {
-        'KAPPA_H': [1.6, 2, 'INFINITY'],
+        'KAPPA_H': [1.6, 2.0, 'INFINITY'],
         'MAX_TERMS': [1000, 1000, 100000],      # The case of kappa_h = inf requires more terms and higher precision
         'MIN_PRECISION': [128, 128, 256],
         'DOUBLE_PRECISION_DELTA': [30, 30, 7],
