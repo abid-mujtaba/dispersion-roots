4660b42e78549195e74808ce18bc12920443c22d
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2017-11-23 16:51:42 +0500

    Switch plots 7-9 to new approach.

--- data/plot-08.R
+++ data/plot-08.R
@@ -1,31 +1,7 @@
-library(rjson)
-library(ggplot2)
+source('custom_themes.R')   # Contains custom_theme and custom_secondary_axis (for tick reflection)
+source('subplots.R')
 
-p <- ggplot()       # Initiate empty plot
-
-
-# We have several series of data so we define a function that takes the file index (name/label) as well the linetype to use for that set and appends the appropriate plots
-subplot <- function(p, index, parameter) {
-
-    # Read data from csv files:
-    DATAFILE <- paste("data-", index, ".csv", sep="")
-    s <- read.csv(DATAFILE)
-
-
-    # Create sub-plots for the sequences
-    for (seq in 1:7) {
-        ss <- s[s$seq == seq,]      # Create sub-set of the data for specified value of 'seq'
-        p <- p + geom_line(data=ss, aes_(x=ss$k_perp, y=ss$omega, linetype=parameter))            # Use 'aes_' to gain access to local variable 'parameter' (scope problems). This requires x= and y= to be declared explicitly. We set 'linetype' equal to the 'parameter' value and later manually provide a conversion from parameter value to the linetype
-    }
-
-
-    # Add title to plot. Note use of expression() to access the plotmath ability to typecast equations/sybmols
-    # and the use of paste() to combine normal text and symbols
-    p <- p +
-            labs(x = expression(k[perp]), y = expression(omega / omega[ce]))
-
-    return (p)
-}
+p <- ggplot()
 
 
 # Repeatedly call subplot to add series of data
@@ -34,14 +10,15 @@ p <- subplot(p, "08-b", "0.3")
 p <- subplot(p, "08-c", "0.5")
 p <- subplot(p, "08-d", "0.8")
 p <- subplot(p, "08-e", "1.0")
-p <- p + scale_linetype_manual(name=expression(frac(n[h0], n[e0])), values=c("0.0"="solid", "0.3"="dotted", "0.5"="dashed", "0.8"="dotdash", "1.0"="longdash")) +      # The 'name' will be the title of the legend
-         ggtitle(expression(paste("Roots of Dispersion Relation for ", kappa[c], " = 2, ", kappa[h], " = 4, ", Lambda, " = 0.01")))
 
-#p <- p + xlim(0,10)        # Limit x-axis values
+# Add title, labels, and customize theme
+p <- p + ggtitle(expression(paste("Roots of Dispersion Relation for ", kappa[c], " = 2, ", kappa[h], " = 4, ", Lambda, " = 0.01")))
+p <- p +
+        scale_x_continuous("$k_\\perp$", sec.axis = custom_secondary_axis, expand  = c(0,0)) +
+        scale_y_continuous("$\\displaystyle \\frac{\\omega}{\\omega_{ce}}$", sec.axis = custom_secondary_axis) +
+        scale_linetype_manual(name="$\\displaystyle \\frac{n_{0h}}{n_{0e}}$", values=c("0.0"="solid", "0.3"="dashed", "0.5"="dotted", "0.8"="dotdash", "1.0"="longdash")) +
+        expand_limits(x = 0) +    # Make graph start at x = 0 (snug up with left margin)
+        custom_theme
 
-
-ggsave(file="plot-08.pdf", plot=p)
-
-
-# Source: http://www.cookbook-r.com/Graphs/Shapes_and_line_types/
-# Source for math expressions: http://vis.supstat.com/2013/04/mathematical-annotation-in-r/
+# write_plot(p, 'plot-08.tex')
+write_plot(p, NULL, F)
--- data/plot-09.R
+++ data/plot-09.R
@@ -1,35 +1,7 @@
-library(rjson)
-library(tikzDevice)
-library(ggplot2)
+source('custom_themes.R')   # Contains custom_theme and custom_secondary_axis (for tick reflection)
+source('subplots.R')
 
-
-# Open the tikz device for output
-tikz(file='plot-09.tex', width=7, height=7)
-
-p <- ggplot() + theme_bw()       # Initiate empty plot
-
-
-# We have several series of data so we define a function that takes the file index (name/label) as well the linetype to use for that set and appends the appropriate plots
-subplot <- function(p, index, parameter) {
-
-    # Read data from csv file:
-
-    DATAFILE <- paste("data-", index, ".csv", sep="")
-    s <- read.csv(DATAFILE)
-
-
-    # Create sub-plots for the sequences
-    for (seq in 1:7) {
-        ss <- s[s$seq == seq,]      # Create sub-set of the data for specified value of 'seq'
-        p <- p + geom_line(data=ss, aes_(x=ss$k_perp, y=ss$omega, linetype=parameter))            # Use 'aes_' to gain access to local variable kappa_h (scope problems). This requires x= and y= to be declared explicitly. We set 'linetype' equal to the 'parameter' value and later manually provide a conversion from parameter value to the linetype
-    }
-
-
-    # Add title to plot. Note use of LaTeX expressions which are quoted and the back-slash is escaped
-    # The .tex output will contain these LaTeX expressions inside the tikz diagram and they will be correctly rendered by pdflatex
-
-    return (p)
-}
+p <- ggplot()
 
 
 # Repeatedly call subplot to add series of data
@@ -37,20 +9,14 @@ p <- subplot(p, "09-a", "10")
 p <- subplot(p, "09-b", "100")
 p <- subplot(p, "09-c", "1000")
 
-p <- p + labs(x = "$k_\\perp$", y = "$\\displaystyle \\frac{\\omega}{\\omega_{ce}}$") +
-         ggtitle("Dispersion for $\\Lambda_c = 0.01$, $\\Lambda_h = 0.1$, $\\kappa_c = 2$, $\\kappa_h = 4$, $\\displaystyle \\frac{n_{0h}}{n_{0e}} = 0.5$")
+# Add title, labels, and customize theme
+p <- p + ggtitle("Dispersion for $\\Lambda_c = 0.01$, $\\Lambda_h = 0.1$, $\\kappa_c = 2$, $\\kappa_h = 4$, $\\displaystyle \\frac{n_{0h}}{n_{0e}} = 0.5$")
+p <- p +
+        scale_x_continuous("$k_\\perp$", sec.axis = custom_secondary_axis, expand  = c(0,0)) +
+        scale_y_continuous("$\\displaystyle \\frac{\\omega}{\\omega_{ce}}$", sec.axis = custom_secondary_axis) +
+        scale_linetype_manual(name="$\\displaystyle \\frac{T_h}{T_c}$", values=c("10"="solid", "100"="dashed", "1000"="dotted")) +
+        expand_limits(x = 0) +    # Make graph start at x = 0 (snug up with left margin)
+        custom_theme
 
-p <- p + scale_linetype_manual(name="$\\displaystyle \\frac{T_h}{T_c}$", values=c("10"="solid", "100"="dashed", "1000"="dotted"))
-
-# Rotate the y-axis title and make its vertical justification centered
-p <- p + theme(axis.title.y = element_text(angle = 0, vjust=0.5))
-
-axis.title = element_text(size = rel(1.5))      # Increase size of axis titles
-
-
-print(p)        # This will create the output using tikzDevice
-dev.off()       # Necessary to close tikzDevice so the .tex file is written
-
-
-# Source: http://www.cookbook-r.com/Graphs/Shapes_and_line_types/
-# Source for tikzdevice: http://iltabiai.github.io/tips/latex/2015/09/15/latex-tikzdevice-r.html
+# write_plot(p, 'plot-06.tex')
+write_plot(p, NULL, F)
