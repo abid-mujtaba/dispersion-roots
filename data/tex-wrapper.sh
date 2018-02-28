79f454b2b01f5312ac42e8d84b6aa815643ad9c5
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2018-02-28 13:08:37 +0500

    Create temporary file (in /tmp/) to wrap the plot TeX files and compile them leaving the TeX files unchanged.

--- data/tex-wrapper.sh
+++ data/tex-wrapper.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 # This script simply adds wrapping lines to the plot tex files to enable standalone compilation and then compiles it
 
+# Copy TeX file to /tmp/ for encapsulation
+cp $1 /tmp/
+
 # Prepend lines using sed. Source: https://www.cyberciti.biz/faq/bash-prepend-text-lines-to-file/
-sed -i '1s;^;\\\documentclass{standalone}\n\\usepackage{tikz}\n\\\begin{document}\n;' $1
+sed -i '1s;^;\\\documentclass{standalone}\n\\usepackage{tikz}\n\\\begin{document}\n;' /tmp/$1
 
 # Append line at end to close document
-sed -i '$ a \\\end{document}' $1
+sed -i '$ a \\\end{document}' /tmp/$1
 
 # Compile the file
-pdflatex $1
+pdflatex /tmp/$1
 
 # Construct pdf file name from tex file name and show it
 pdf=$(echo $1 | sed 's/\..*$/\.pdf/')
