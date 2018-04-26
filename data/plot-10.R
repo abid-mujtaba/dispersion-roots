117d06e494936eb1b5bf914aceef0cd7661724ac
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2018-04-26 16:35:28 +0500

    Correct label of distribution.

--- data/plot-10.R
+++ data/plot-10.R
@@ -7,7 +7,7 @@ p <- ggplot()
 
 
 # Repeatedly call subplot to add series of data
-p <- subplot(p, "10-a", "Maxwellian")
+p <- subplot(p, "10-a", "Bi-Maxwellian")
 p <- subplot(p, "10-b", "Double-Kappa")
 p <- subplot(p, "10-c", "Cairns")
 p <- subplot(p, "10-d", "VC")
@@ -17,7 +17,7 @@ p <- subplot(p, "10-d", "VC")
 p <- p +
         scale_x_continuous("$k_\\perp \\rho_h$", sec.axis = custom_secondary_axis, expand  = c(0,0)) +
         scale_y_continuous("$\\displaystyle \\frac{\\omega}{\\omega_{ce}}$", sec.axis = custom_secondary_axis) +
-        scale_linetype_manual(name="Distribution", values=c("Maxwellian"="solid", "Double-Kappa"="dashed", "Cairns"="dotted", "VC"="dotdash")) +
+        scale_linetype_manual(name="Distribution", values=c("Bi-Maxwellian"="solid", "Double-Kappa"="dashed", "Cairns"="dotted", "VC"="dotdash")) +
         expand_limits(x = 0) +    # Make graph start at x = 0 (snug up with left margin)
         custom_theme
 
--- data/plot-12.R
+++ data/plot-12.R
@@ -7,7 +7,7 @@ p <- ggplot()
 
 
 # Repeatedly call subplot to add series of data
-p <- subplot(p, "12-a", "Maxwellian")
+p <- subplot(p, "12-a", "Bi-Maxwellian")
 p <- subplot(p, "12-b", "Double-Kappa")
 p <- subplot(p, "12-c", "Cairns")
 p <- subplot(p, "12-d", "VC")
@@ -17,7 +17,7 @@ p <- subplot(p, "12-d", "VC")
 p <- p +
         scale_x_continuous("$k_\\perp \\rho_h$", sec.axis = custom_secondary_axis, expand  = c(0,0)) +
         scale_y_continuous("$\\displaystyle \\frac{\\omega}{\\omega_{ce}}$", sec.axis = custom_secondary_axis) +
-        scale_linetype_manual(name="Distribution", values=c("Maxwellian"="solid", "Double-Kappa"="dashed", "Cairns"="dotted", "VC"="dotdash")) +
+        scale_linetype_manual(name="Distribution", values=c("Bi-Maxwellian"="solid", "Double-Kappa"="dashed", "Cairns"="dotted", "VC"="dotdash")) +
         expand_limits(x = 0) +    # Make graph start at x = 0 (snug up with left margin)
         custom_theme
 
