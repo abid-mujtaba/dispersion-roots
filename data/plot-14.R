7bfdd9573d392dc89617ce75b3d3d5fb61b8b79d
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2018-05-08 11:55:34 +0500

    Generate data and plots for Kappa_c = 2 varying Lambdas

---
+++ data/plot-14.R
@@ -0,0 +1,23 @@
+source('custom_themes.R')   # Contains custom_theme and custom_secondary_axis (for tick reflection)
+source('subplots.R')
+
+filename <- 'plot-14.tex'
+
+p <- ggplot()
+
+# Repeatedly call subplot to add series of data
+p <- subplot(p, "14-a", "0.00")
+p <- subplot(p, "14-b", "0.01")
+p <- subplot(p, "14-c", "0.10")
+p <- subplot(p, "14-d", "0.25")
+
+# Add title, labels, and customize theme
+#p <- p + ggtitle("Dispersion for $\\Lambda_h = 0.2$, $\\kappa_c = 2$, $\\kappa_h = 4$, $\\frac{n_{0h}}{n_{0e}} = 0.5$")
+p <- p +
+        scale_x_continuous("$k_\\perp \\rho_h$", sec.axis = custom_secondary_axis, expand  = c(0,0)) +
+        scale_y_continuous("$\\displaystyle \\frac{\\omega}{\\omega_{ce}}$", sec.axis = custom_secondary_axis) +
+        scale_linetype_manual(name="$\\Lambda_c$", values=c("0.00"="solid", "0.01"="dotted", "0.10"="2282", "0.25"="dashed")) +
+        expand_limits(x = 0) +    # Make graph start at x = 0 (snug up with left margin)
+        custom_theme
+
+write_plot(p, filename, texOutput = T)
---
+++ generate/data_13.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+#
+# Script for generating data for plot 02 (different values of KAPPA_H)
+
+
+from data_base import set_defaults, set_roots_value, set_data_value, iterate_variables
+
+
+PLOT = "13"
+VARIABLES = {
+    'LAMBDA_H': [0.00, 0.01, 0.1, 0.25],
+}
+
+
+# We first declare the default values we want to set as a dictionary
+defaults = {
+        'LAMBDA_C': 0.01,
+        'KAPPA_C': 2.0,
+        'KAPPA_H': 4.0,
+        'N0H_BY_N0E': 0.5,
+        'TH_BY_TC': 101.695
+    }
+
+K_PERP_MAX = 20
+
+
+
+# Apply changes and generate data
+set_roots_value("K_PERP_MAX", K_PERP_MAX)
+set_defaults(defaults)
+iterate_variables(PLOT, VARIABLES)
